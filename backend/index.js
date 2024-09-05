f3c3ab00c529505e58b9db387bd27d75e4719d61
jared-yu-hcltech <jared.yu@hcltech.com>
2024-09-05 14:24:58 -0700

    Update index.js
    Remove extra import

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,6 @@ import mongoose from "mongoose";
 import Chat from "./models/chat.js";
 import UserChats from "./models/userChats.js";
 import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node'
-import exp from "constants";
 
 const port = process.env.PORT || 3000;
 const app = express();
