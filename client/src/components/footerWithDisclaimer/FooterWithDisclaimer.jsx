fb749b1d038358a0801c9d37e0729070ffa40daf
jared-yu-hcltech <jared.yu@hcltech.com>
2024-09-14 11:53:23 -0700

    Update FooterWithDisclaimer.jsx
    Fix typo in Disclaimer

--- client/src/components/footerWithDisclaimer/FooterWithDisclaimer.jsx
+++ client/src/components/footerWithDisclaimer/FooterWithDisclaimer.jsx
@@ -4,7 +4,7 @@ import './footerWithDisclaimer.css';
 const FooterWithDisclaimer = () => {
     return (
         <div className="disclaimer">
-            Caution: The chatbot may make inaccurate or misleading statements, make sure to verrify the results.
+            Caution: The chatbot may make inaccurate or misleading statements, make sure to verify the results.
         </div>
     );
 };
