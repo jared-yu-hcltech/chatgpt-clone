489e171b28418a2d86cc6a3664390e652b4a6112
jared-yu-hcltech <jared.yu@hcltech.com>
2024-09-16 11:45:43 -0700

    Add the template for current model
    Adding initial code to show the current model to the navbar. Makes it so that it shows it when in the /dashboard/ path. #23

--- client/src/layouts/rootLayout/RootLayout.jsx
+++ client/src/layouts/rootLayout/RootLayout.jsx
@@ -1,17 +1,21 @@
-import { Link, Outlet } from 'react-router-dom'
-import './rootLayout.css'
+import { Link, Outlet, useLocation } from 'react-router-dom';
+import './rootLayout.css';
 import { SignedIn, UserButton, ClerkProvider } from "@clerk/clerk-react";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
+  throw new Error("Missing Publishable Key");
 }
 
 const queryClient = new QueryClient();
 
-const RootLayout = () => {
+const RootLayout = ({ chatbotModel }) => {
+  const location = useLocation();
+  
+  const isDashboardRoute = location.pathname.startsWith("/dashboard");
+
   return (
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <QueryClientProvider client={queryClient}>
@@ -21,6 +25,9 @@ const RootLayout = () => {
               <img src='/hcltechicon.png' alt='' />
               <span>Gen AI Chatbot</span>
             </Link>
+            <div className="navbar-extra">
+              {isDashboardRoute && <span>Current Model: {chatbotModel}</span>}
+            </div>
             <div className="user">
               <SignedIn>
                 <UserButton />
@@ -33,7 +40,7 @@ const RootLayout = () => {
         </div>
       </QueryClientProvider>
     </ClerkProvider>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
